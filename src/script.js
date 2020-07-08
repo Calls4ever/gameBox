cf07cfa2a2a5b0aac12385436c79dafc2172493a
callas4ever <thetseringnorbu@gmail.com>
2020-07-08 17:27:02 -0400

    star rating js, html and css

--- src/script.js
+++ src/script.js
@@ -187,10 +187,18 @@ const renderGame=(game, userGame)=>{
       <h7 class='card-text'>Genre: <p> ${game.genre}</p> </h7>
       <h6 class='card-text' id= "rating-game">Critics Rating: ${showRating(game.rating)}</h6>
       <br>
-      <h7 class='card-text'>${Math.round(userGame.time_played/(parseInt(game.playtime)*60)*100)}% completed!</h7>
+      <div id='rating'>
+      <h7 class='card-text'>Rate this game </h7>
+      <div class="rating">
+      
+        <span>☆</span><span>☆</span><span>☆</span><span>☆</span><span>☆</span>
+      </div>
+      </div>
+      ${completeGame(game.playtime, userGame.time_played)}
+
       <div class="progress">
-        <div class="progress-bar progress-bar-striped progress-bar-animated" role="progressbar" aria-valuenow="${userGame.time_played}" aria-valuemin="0" aria-valuemax="${parseInt(game.playtime)*60}" style="width: ${userGame.time_played/(parseInt(game.playtime)*60)*100}%"></div>
-        <span class="progress-btn"><button>Add</button> </span></div>
+        <div class="progress-bar progress-bar-lg progress-bar-striped progress-bar-animated" role="progressbar" aria-valuenow="${userGame.time_played}" aria-valuemin="0" aria-valuemax="${parseInt(game.playtime)*60}" style="width: ${userGame.time_played/(parseInt(game.playtime)*60)*100}%"></div>
+        <span class="progress-btn"><button class='btn btn-outline-success btn-sm' id='${userGame.id}'></button> </span></div>
         <br>
       <a id='${game.id}' class="btn btn-danger" style='color: white'>Remove Game</a>
       
@@ -229,4 +237,13 @@ const renderUserProfile=user=>{
     profileDiv=document.querySelector('#user-profile')
     profileDiv.innerHTML=`<img id='profile-pic' src='${user.profile_pic}'>
                           <h2 class='display-4' id='profile-name'>${user.name}</h2>`
+}
+
+const completeGame=(gTime, pTime)=>{
+    if(parseInt(gTime)==pTime){
+        return `<h7 class='card-text'style="color:green">100% Completed<svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-check" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+        <path fill-rule="evenodd" d="M10.97 4.97a.75.75 0 0 1 1.071 1.05l-3.992 4.99a.75.75 0 0 1-1.08.02L4.324 8.384a.75.75 0 1 1 1.06-1.06l2.094 2.093 3.473-4.425a.236.236 0 0 1 .02-.022z"/>
+      </svg></h7>`
+    }else return `${Math.round(pTime/(parseInt(gTime)*60)*100)}% completed`
+    
 }
